093bed7a3539d74acc85d31848f4245629758e8a
richintheflow <richardg@posteo.net>
2022-04-19 19:54:49 +0200

    Update and finish exercise 03_reverseString/

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  // let str = ['h','e','l','l','o'];
+  return string.split('').reverse().join('');
 };
-
 // Do not edit below this line
 module.exports = reverseString;
